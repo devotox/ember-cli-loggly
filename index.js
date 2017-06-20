311e8630107a17f02a3e4a9e4e5acbfbad984959
Devonte <devo.tox.89@gmail.com>
2017-06-20 14:50:11 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 /* eslint-env node */
-'use strict';
+
+const path = require('path');
+const Funnel = require('broccoli-funnel');
+const MergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
-  name: 'ember-cli-loggly'
+  name: 'ember-cli-loggly',
+  included() {
+    this._super.included.apply(this, arguments);
+    this._ensureThisImport();
+
+    this.import('vendor/loggly.tracker.js');
+    this.import('vendor/shims/loggly.js');
+  },
+  treeForVendor(vendorTree) {
+    let logglyTree = new Funnel(path.dirname(require.resolve('loggly-jslogger/src/loggly.tracker.js')), {
+      files: ['loggly.tracker.js'],
+    });
+
+    return new MergeTrees([vendorTree, logglyTree]);
+  },
+  _ensureThisImport() {
+    if (!this.import) {
+      this._findHost = function findHostShim() {
+        let current = this;
+        let app;
+
+        do {
+          app = current.app || app;
+        } while (current.parent.parent && (current = current.parent));
+        return app;
+      };
+      this.import = function importShim(asset, options) {
+        let app = this._findHost();
+        app.import(asset, options);
+      };
+    }
+  }
 };
+
+
---
+++ vendor/shims/loggly.js
@@ -0,0 +1,12 @@
+(function() {
+  function vendorModule() {
+    'use strict';
+
+    return {
+      'default': self['_LTracker'],
+      __esModule: true,
+    };
+  }
+
+  define('loggly', [], vendorModule);
+})();
