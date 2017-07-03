7c46a1aa695bb3cc898beb50bf7e207df4dc3702
Devonte <devo.tox.89@gmail.com>
2017-07-03 01:26:56 +0100

    fastboot ready

--- index.js
+++ index.js
@@ -1,42 +1,44 @@
 /* eslint-env node */
 
-const path = require('path');
-const Funnel = require('broccoli-funnel');
-const MergeTrees = require('broccoli-merge-trees');
+var FastbootTransform = require('fastboot-transform');
 
 module.exports = {
   name: 'ember-cli-loggly',
-  included() {
-    this._super.included.apply(this, arguments);
-    this._ensureThisImport();
-
-    this.import('vendor/loggly.tracker.js');
-    this.import('vendor/shims/loggly.js');
+  options: {
+	nodeAssets: {
+	  'loggly-jslogger': {
+		vendor: {
+		  srcDir: 'src',
+		  include: ['loggly.tracker.js'],
+		  processTree(input) {
+			return FastbootTransform(input);
+		  }
+		}
+	  }
+	}
   },
-  treeForVendor(vendorTree) {
-    let logglyTree = new Funnel(path.dirname(require.resolve('loggly-jslogger/src/loggly.tracker.js')), {
-      files: ['loggly.tracker.js'],
-    });
+  included() {
+	this._super.included.apply(this, arguments);
+	this._ensureThisImport();
 
-    return new MergeTrees([vendorTree, logglyTree]);
+	this.import ('vendor/loggly-jslogger/loggly.tracker.js');
+	this.import ('vendor/shims/loggly.js');
   },
   _ensureThisImport() {
-    if (!this.import) {
-      this._findHost = function findHostShim() {
-        let current = this;
-        let app;
+	if (!this.import) {
+	  this._findHost = function findHostShim() {
+		let current = this;
+		let app;
 
-        do {
-          app = current.app || app;
-        } while (current.parent.parent && (current = current.parent));
-        return app;
-      };
-      this.import = function importShim(asset, options) {
-        let app = this._findHost();
-        app.import(asset, options);
-      };
-    }
+		do {
+		  app = current.app || app;
+		} while (current.parent.parent && (current = current.parent));
+		return app;
+	  };
+	  this.import = function importShim(asset, options) {
+		let app = this._findHost();
+		app.import (asset, options);
+	  };
+	}
   }
 };
-
-
